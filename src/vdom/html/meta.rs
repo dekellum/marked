9d743635d4a4663c9a6fe4c129effe0206524400
David Kellum <dek-oss@gravitext.com>
2019-09-30 11:22:04 -0700

    minimized manual tag/attr lname constants for now

--- src/vdom/html/meta.rs
+++ src/vdom/html/meta.rs
@@ -2,108 +2,29 @@
 
 #![allow(unused)]
 
-use std::collections::{HashMap};
-
-use crate::vdom::LocalName;
-use lazy_static::lazy_static;
-
-pub struct TagMeta {
-    is_empty: bool,
-    is_deprecated: bool,
-    is_inline: bool,
-    is_meta: bool,
-    is_banned: bool,
-    basic_attrs: Vec<LocalName>,
-}
-
-impl TagMeta {
-    pub fn is_empty(&self) -> bool {
-        self.is_empty
-    }
-    pub fn is_deprecated(&self) -> bool {
-        self.is_deprecated
-    }
-    pub fn is_inline(&self) -> bool {
-        self.is_inline
-    }
-    pub fn is_meta(&self) -> bool {
-        self.is_meta
-    }
-    pub fn is_banned(&self) -> bool {
-        self.is_banned
-    }
-
-    pub fn has_basic_attr(&self, name: &LocalName) -> bool {
-        self.basic_attrs.binary_search(name).is_ok()
-    }
-}
-
-impl Default for TagMeta {
-    fn default() -> TagMeta {
-        TagMeta {
-            is_empty: false,
-            is_deprecated: false,
-            is_inline: false,
-            is_meta: false,
-            is_banned: false,
-            basic_attrs: vec![],
-        }
-    }
-}
-
-lazy_static! {
-    /// A static lookup table for metadata on known HTML tags.
-    pub static ref TAG_META: HashMap<LocalName, TagMeta> = init_tag_metadata();
-}
-
-fn init_tag_metadata() -> HashMap<LocalName, TagMeta> {
-    let mut tag_meta = HashMap::new();
-
-    let mut basic_attrs = vec![
-        a::HREF, a::REL, a::ID, a::CLASS, a::STYLE, a::TARGET, a::TITLE,
-        a::EXPORTPARTS.clone() // Just for testing
-    ];
-    basic_attrs.sort();
-    basic_attrs.dedup();
-
-    tag_meta.insert(t::A, TagMeta {
-        is_inline: true,
-        basic_attrs,
-        .. TagMeta::default()
-    });
-
-    tag_meta
-}
-
+/// HTML tag constants
 pub mod t {
     use crate::vdom::{LocalName, lname};
 
-    /// The <a> anchor tag
     pub const A: LocalName = lname!("a");
+    pub const BODY: LocalName = lname!("body");
+    pub const HEAD: LocalName = lname!("head");
+    pub const META: LocalName = lname!("meta");
 }
 
+/// HTML attribute constants
 pub mod a {
     use crate::vdom::{LocalName, lname};
 
     pub const CLASS:          LocalName = lname!("class");
+    pub const CHARSET:        LocalName = lname!("charset");
+    pub const CONTENT:        LocalName = lname!("content");
     pub const HREF:           LocalName = lname!("href");
+    pub const HTTP_EQUIV:     LocalName = lname!("http-equiv");
     pub const ID:             LocalName = lname!("id");
+    pub const ITEMREF:        LocalName = lname!("itemref");
     pub const REL:            LocalName = lname!("rel");
     pub const STYLE:          LocalName = lname!("style");
     pub const TARGET:         LocalName = lname!("target");
     pub const TITLE:          LocalName = lname!("title");
-
-    pub const ITEMREF:        LocalName = lname!("itemref");
-
-    lazy_static::lazy_static! {
-        pub static ref EXPORTPARTS: LocalName = "exportparts".into();
-    }
-}
-
-#[test]
-fn lookup_basics() {
-    let tmeta = TAG_META.get(&t::A).unwrap();
-    assert!(tmeta.is_inline());
-    assert!(tmeta.has_basic_attr(&a::TARGET));
-    assert!(tmeta.has_basic_attr(&a::EXPORTPARTS));
 }
