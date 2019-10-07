c6c8573aa0366148b2837553f11daa6d0cc02f07
David Kellum <dek-oss@gravitext.com>
2019-10-07 10:08:20 -0700

    use tag/attr/ns constants throughout, drop macro re-exports

--- src/vdom/html/meta.rs
+++ src/vdom/html/meta.rs
@@ -2,19 +2,38 @@
 
 #![allow(unused)]
 
+/// *ML `Namespace` constants
+pub mod ns {
+    use html5ever::ns;
+    use crate::vdom::Namespace;
+
+    pub const HTML:           Namespace = ns!(html);
+}
+
 /// HTML tag constants
 pub mod t {
-    use crate::vdom::{LocalName, lname};
+    use html5ever::local_name as lname;
+    use crate::vdom::LocalName;
 
-    pub const A: LocalName = lname!("a");
-    pub const BODY: LocalName = lname!("body");
-    pub const HEAD: LocalName = lname!("head");
-    pub const META: LocalName = lname!("meta");
+    // FIXME: For now this is an incomplete list
+
+    pub const A:              LocalName = lname!("a");
+    pub const BODY:           LocalName = lname!("body");
+    pub const DIV:            LocalName = lname!("div");
+    pub const HEAD:           LocalName = lname!("head");
+    pub const HTML:           LocalName = lname!("html");
+    pub const META:           LocalName = lname!("meta");
+    pub const P:              LocalName = lname!("p");
+    pub const STRIKE:         LocalName = lname!("strike");
 }
 
 /// HTML attribute constants
 pub mod a {
-    use crate::vdom::{LocalName, lname};
+    use html5ever::local_name as lname;
+    use crate::vdom::LocalName;
+
+    // FIXME: For now this is an incomplete list and lacks any association with
+    // elements
 
     pub const CLASS:          LocalName = lname!("class");
     pub const CHARSET:        LocalName = lname!("charset");
