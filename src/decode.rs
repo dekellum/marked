fe82bfccd29e116cc0f68729b9f4763e647942bd
David Kellum <dek-oss@gravitext.com>
2019-11-11 13:08:17 -0800

    move EncodingHint to its own file

--- src/decode.rs
+++ src/decode.rs
@@ -11,9 +11,6 @@
 //! Support for streaming charset decoding.
 
 use std::borrow::Cow;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
 
 use encoding_rs as enc;
 use enc::DecoderResult;
@@ -22,125 +19,12 @@ use tendril::{Tendril, TendrilSink, Atomicity, NonAtomic};
 use tendril::fmt as form;
 use tendril::stream::Utf8LossyDecoder;
 
-pub struct EncodingHint {
-    encodings: HashMap<&'static enc::Encoding, f32>,
-    top: Option<&'static enc::Encoding>,
-    confidence: f32,
-    changed: bool,
-}
+mod encoding_hint;
 
-/// Recommended confidence for any default charset
-pub const DEFAULT_CONF: f32       = 0.01;
-
-/// Recommended confidence for a hint from an HTTP content-type header with
-/// charset.
-pub const HTTP_CTYPE_CONF: f32    = 0.09;
-
-/// Recommended confidence for the sum of all hints from within an HTML head,
-/// in meta elements.
-pub const HTML_META_CONF: f32     = 0.20;
-
-/// An `EncodingHint` that can be shared between `Decoder` and `Sink`, by
-/// reference on the same thread, and internally mutated. The type is neither
-/// `Send` nor `Sync`.
-pub type SharedEncodingHint = Rc<RefCell<EncodingHint>>;
-
-impl EncodingHint {
-    /// Construct new, empty EncodingHint.
-    pub fn new() -> EncodingHint {
-        EncodingHint {
-            encodings: HashMap::new(),
-            top: None,
-            confidence: 0.0,
-            changed: false,
-        }
-    }
-
-    /// Construct a new Encoding hint with the specified encoding at
-    /// `DEFAULT_CONF` confidence, wrapped for sharing.
-    pub fn shared_default(enc: &'static enc::Encoding) -> SharedEncodingHint {
-        let mut eh = EncodingHint::new();
-        eh.add_hint(enc, DEFAULT_CONF);
-        eh.clear_changed();
-        Rc::new(RefCell::new(eh))
-    }
-
-    /// Add a hint for an encoding, by label ASCII-intepreted bytes, and some
-    /// positive confidence value.  If no encoding (or applicable replacement)
-    /// is found for the specified label, returns false.  Return true if an
-    /// encoding is found _and_ this hint changes the top confidence encoding.
-    pub fn add_label_hint<L>(&mut self, enc: L, confidence: f32)
-        -> bool
-        where L: AsRef<[u8]>
-    {
-        if let Some(enc) = enc::Encoding::for_label(enc.as_ref()) {
-            self.add_hint(enc, confidence)
-        } else {
-            false
-        }
-    }
-
-    /// Add a hint for the specified encoding and some positive confidence
-    /// value. Return true if this hint changes the top most confident
-    /// encoding.
-    pub fn add_hint(&mut self, enc: &'static enc::Encoding, confidence: f32)
-        -> bool
-    {
-        assert!(confidence > 0.0);
-
-        let new_conf = *(
-            self.encodings.entry(enc)
-                .and_modify(|c| *c += confidence)
-                .or_insert(confidence)
-        );
-
-        if new_conf > self.confidence {
-            self.confidence = new_conf;
-            if self.top == Some(enc) {
-                false
-            } else {
-                self.top = Some(enc);
-                self.changed = true;
-                true
-            }
-        } else {
-            false
-        }
-    }
-
-    /// Return the top (most confident) encoding, if at least one encoding has
-    /// been hinted.
-    pub fn top(&self) -> Option<&'static enc::Encoding> {
-        self.top
-    }
-
-    /// Return the summed confidence value for the top (most confident)
-    /// encoding. Returns 0.0 if no hint has been provided.
-    pub fn confidence(&self) -> f32 {
-        self.confidence
-    }
-
-    /// Return the latest top encoding if the top has changed since
-    /// construction or the last call to `clear_changed`.
-    pub fn changed(&self) -> Option<&'static enc::Encoding> {
-        if self.changed {
-            self.top
-        } else {
-            None
-        }
-    }
-
-    /// Clear changed state.
-    pub fn clear_changed(&mut self) {
-        self.changed = false
-    }
-}
-
-impl Default for EncodingHint {
-    fn default() -> EncodingHint {
-        EncodingHint::new()
-    }
-}
+pub use encoding_hint::{
+    EncodingHint, SharedEncodingHint,
+    DEFAULT_CONF, HTML_META_CONF, HTTP_CTYPE_CONF,
+};
 
 /// A `TendrilSink` adaptor that takes bytes, decodes them as the given
 /// character encoding, lossily replace ill-formed byte sequences with U+FFFD
@@ -391,46 +275,5 @@ mod tests {
             let decoder = Decoder::new(enc::EUC_KR, Accumulate::new());
             check_decode(decoder, input, expected, errs);
         }
-    }
-
-    #[test]
-    fn encoding_hint() {
-        let mut encs = EncodingHint::new();
-        assert!( encs.add_label_hint("LATIN1",     0.3));
-        assert!(!encs.add_label_hint("iso-8859-1", 0.4));
-        assert!(!encs.add_label_hint("utf-8",      0.5));
-        assert_eq!(
-            "windows-1252", encs.top().unwrap().name(),
-            "desired replacement for first two hints"
-        );
-        assert_eq!(0.3 + 0.4, encs.confidence());
-    }
-
-    fn is_send<T: Send>() -> bool { true }
-    fn is_sync<T: Sync>() -> bool { true }
-
-    #[test]
-    fn test_send_sync() {
-        assert!(is_send::<EncodingHint>());
-        assert!(is_sync::<EncodingHint>());
-    }
-
-    // Adapted from static_asserts 1.1.0 `assert_not_impl_any` macro
-    // MIT/Apache licensed
-
-    trait AmbiguousIfImpl<A> {
-        fn some_f() -> bool { true }
-    }
-    impl<T: ?Sized> AmbiguousIfImpl<()> for T {}
-
-    #[allow(unused)] struct NotSync;
-    impl<T: ?Sized + Sync> AmbiguousIfImpl<NotSync> for T {}
-
-    #[allow(unused)] struct NotSend;
-    impl<T: ?Sized + Send> AmbiguousIfImpl<NotSend> for T {}
-
-    #[test]
-    fn test_not_send_nor_sync() {
-        assert!(<SharedEncodingHint as AmbiguousIfImpl<_>>::some_f());
     }
 }
