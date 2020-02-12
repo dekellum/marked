ed6ec18184e07bad6b1d7a685e2bfbed11f8d57a
David Kellum <dek-oss@gravitext.com>
2020-02-12 10:03:46 -0800

    cosmetic

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,16 @@
 
 #[macro_use] extern crate html5ever;
 
+// Default parse buffer size
 const PARSE_BUFFER_SIZE: u32 = 4 * 1024;
+
+mod chars;
+
+mod decode;
+pub use decode::{
+    Decoder, EncodingHint, SharedEncodingHint,
+    DEFAULT_CONF, HTML_META_CONF, HTTP_CTYPE_CONF
+};
 
 mod vdom;
 pub use vdom::{
@@ -11,14 +20,6 @@ pub use vdom::{
     Attribute, LocalName, Namespace, QualName, StrTendril,
     XmlError
 };
-
-mod decode;
-pub use decode::{
-    Decoder, EncodingHint, SharedEncodingHint,
-    DEFAULT_CONF, HTML_META_CONF, HTTP_CTYPE_CONF
-};
-
-mod chars;
 
 #[cfg(test)]
 mod logger;
